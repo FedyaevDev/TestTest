770748468767f0d9e1716b128ae32da8e8890621
FedayevR <fedayevDev@yandex.ru>
2023-04-26 01:07:44 +0700

    Added for

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp28
     {
         static void Main(string[] args)
         {
-
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello\n");
+            }
         }
     }
 }
